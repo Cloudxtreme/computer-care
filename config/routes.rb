12949bc76bb0678f1eabc56c59868f7af8352bc8
Jason <jsofokleous@googlemail.com>
2013-07-16 00:12:02 +0100

    added markup for the jquery slider

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 MarketingSite::Application.routes.draw do
   root to: "pages#home"
   match "home", to: "pages#home"
-
+  match "services", to: "pages#services"
+  match "contact", to: "pages#contact"
+  match "about", to: "pages#about"
+  match "offera", to: "pages#offers"
+  match "faq", to: "pages#faq"
+  match "terms", to: "pages#terms"
   resources :companies
 end
