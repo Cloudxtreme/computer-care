6ce754165ee40b6860fd0fd638dc6b29d00e4026
Jason <jsofokleous@googlemail.com>
2013-07-16 20:26:46 +0100

    get the navigation links to display the correct state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
             "$"
         end
     end
+
+    def is_active(page)
+    	"active" if controller.action_name.eql?(page)
+    end
+
+    def home_link
+    	if controller.action_name.eql?("home")
+    		link_to image_tag("home-active.png"), root_path, :id => "home-link", :class => "active"
+    	else
+    		link_to image_tag("home.png"), root_path, :id => "home-link"
+    	end
+    end
 end
