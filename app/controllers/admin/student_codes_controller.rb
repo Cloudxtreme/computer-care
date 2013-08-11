13324d501017f8611fd7ef0c440602510bc6045b
Jason <jsofokleous@googlemail.com>
2013-08-11 11:03:10 +0100

    student discount codes can be made inactive

--- app/controllers/admin/student_codes_controller.rb
+++ app/controllers/admin/student_codes_controller.rb
@@ -1,5 +1,13 @@
-class Admin::NewsletterUsersController < Admin::AdminController
+class Admin::StudentCodesController < Admin::AdminController
   def index
-    @students = StudentCodes.all
+    @students = StudentCode.all
+  end
+
+  def destroy
+    @discount = StudentCode.find(params[:id])
+    #@discount.code = nil
+    @discount.is_valid = false
+    @discount.save
+    redirect_to admin_student_codes_path
   end
 end
