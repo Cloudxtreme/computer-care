79c978cd11b1588f88800aaeb0dfd0c6cafe8c40
Jason <jsofokleous@googlemail.com>
2013-08-11 11:02:16 +0100

    make sure that the same email can't be subscribed to the newsletter multiple times

--- app/controllers/newsletter_users_controller.rb
+++ app/controllers/newsletter_users_controller.rb
@@ -1,11 +1,15 @@
 class NewsletterUsersController < ApplicationController
   def create
-    user = NewsletterUser.new(params[:newsletter_user])
-    if user.save
-      NotificationMailer.newsletter_signup(user.email).deliver
-      redirect_to root_path, :notice => "Thanks for signing up!"
+    if NewsletterUser.where("email" => params[:newsletter_user][:email]).count == 0
+      user = NewsletterUser.new(params[:newsletter_user])
+      if user.save
+        NotificationMailer.newsletter_signup(user.email).deliver
+        redirect_to root_path, :notice => "Thanks for signing up!"
+      else
+        redirect_to root_path, :notice => "Something went wrong, please try again making sure you use a valid email address."
+      end
     else
-      redirect_to root_path, :notice => "Something went wrong, please try again making sure you use a valid email address."
+      redirect_to root_path, :notice => "Thanks for signing up!"
     end
   end
 end
--- app/models/newsletter_user.rb
+++ app/models/newsletter_user.rb
@@ -1,4 +1,5 @@
 class NewsletterUser < ActiveRecord::Base
   attr_accessible :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_uniqueness_of :email
 end
