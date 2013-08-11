71cd40fc8ff6f501fb1d074e82ccd47855fe6cd2
Jason <jsofokleous@googlemail.com>
2013-08-11 11:01:27 +0100

    can remove users subscribed to the newsletter

--- app/controllers/admin/newsletter_users_controller.rb
+++ app/controllers/admin/newsletter_users_controller.rb
@@ -2,4 +2,10 @@ class Admin::NewsletterUsersController < Admin::AdminController
   def index
     @users = NewsletterUser.all
   end
+
+  def destroy
+    user = NewsletterUser.find(params[:id])
+    user.destroy
+    redirect_to admin_newsletter_users_path
+  end
 end
