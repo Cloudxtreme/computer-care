e2e13408eb0e585853169cdadc76daf7736795cf
Jason <jsofokleous@googlemail.com>
2013-08-18 22:10:40 +0100

    added customer details to order form and validation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,21 @@
 class OrdersController < ApplicationController
   def new
     @services = Service.all
+    @missing = []
   end
 
   def create
+    @services = Service.all
+    required = ["first-name", "last-name", "email", "telephone", "buidling", "street", "town", "postcode"]
+    @missing = []
+    required.each do |param|      
+      @missing << param if params[param].blank?
+    end
+
+    if @missing.any?
+      render :new
+      #redirect_to new_order_path, :notice => ""
+    else
+    end
   end
 end
