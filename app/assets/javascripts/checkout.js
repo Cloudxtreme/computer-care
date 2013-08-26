94d74ac74809bb9142f60b60e68e77f415012caf
Jason <jsofokleous@googlemail.com>
2013-08-26 11:49:26 +0100

    order form javascript

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -1,10 +1,43 @@
 jQuery(function($){
+  // accordian effect
+  initialize_form();
+  $("#order-form").on("click", "fieldset.active a.next", function() {
+    $fieldset = $(this).closest("fieldset");
+    if($fieldset.attr("id") == "options") {
+      // make sure atleast one service was selected
+      if($("input[type='checkbox']:checked", $fieldset).length > 0) {
+        open_next_section($fieldset);
+      }
+    }
+    else if($fieldset.attr("id") == "user") {
+      // make sure all fields were filled out
+      var can_continue = true;
+      $("input[type='text']", $fieldset).each(function() {
+        if(!$(this).val()) {
+          can_continue = false;
+        }
+      });
+      if(can_continue) {
+        open_next_section($fieldset);
+      }
+    }
+    else if($fieldset.attr("id") == "collection") {
+      open_next_section($fieldset);
+    } 
+  });
+
   $(".service").click(function() {
     if($(".service:checked[data-can-checkout='false']").length > 0) {
-      $("#payment").hide();
+      // make payment form disabled
+      $("#payment input[type='text']").attr("disabled", "disabled").hide();
+      //show message
+      $("#payment .alert").show();
     }
     else {
-      $("#payment").show(); 
+      // enable payment form 
+      $("#payment input[type='text']").removeAttr("disabled").show();
+      // hide message
+      $("#payment .alert").hide();
     }
   });
 
@@ -33,3 +66,33 @@ jQuery(function($){
     }
   });
 });
+
+function initialize_form() {
+  $("#order-form fieldset").first().addClass("active");
+  $("#order-form fieldset:not(:first)").hide();
+  $("#order-form header.stripe").each(function(i) {
+    i++;
+    if(i>2) {
+      $(this).find(".img-shadow.left").hide();
+      $(this).find(".img-shadow.right").hide();
+    }
+    else if(i==2) {
+      $(this).find(".img-shadow.left:not(.top)").hide();
+      $(this).find(".img-shadow.right:not(.top)").hide();
+    }
+  });
+  $("#order-form footer.stripe .img-shadow").hide();  
+}
+
+function open_next_section($current) {
+  $current.hide();
+  $current.prev("header").find(".img-shadow").hide();
+  $current.next().next("fieldset").addClass("active").show();
+  $current.next("header").find(".img-shadow").show();  
+  $current.next("header").find(".img-shadow.top").hide();
+  $current.next().next().next().find(".img-shadow.top").show();
+  $current.removeClass("active");
+  if($("fieldset.active").is(":nth-last-child(2)")) {
+    $("footer.stripe .img-shadow").show();
+  }
+}
