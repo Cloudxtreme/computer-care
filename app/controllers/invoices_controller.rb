5b42d297c6c3b98543f499e3e8e4dec4279b40d2
Jason <jsofokleous@googlemail.com>
2013-10-17 08:58:13 +0100

    get date and accept terms on invoice payment

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,14 +2,31 @@ class InvoicesController < ApplicationController
   def show
     @order = Order.find(params[:order_id])
     @invoice = @order.invoice
+    date = Date.today
+    timezone = ActiveSupport::TimeZone['London']    
+    time = timezone.local(date.year, date.month, date.day) + 13.hours
+    @can_collect_today = Time.now < time    
   end
 
   def payment
     @order = Order.find(params[:order_id])
     @invoice = @order.invoice
-    if false #payment successful
+    @accepted = params[:accept_terms] && params[:accept_terms].eql?("on") ? true : false
+
+    date = Date.today
+    timezone = ActiveSupport::TimeZone['London']    
+    time = timezone.local(date.year, date.month, date.day) + 13.hours
+    @can_collect_today = Time.now < time
+
+    date = params["date"].split("/") if params["date"]
+    delivery_date = Time.new(date.last, date.first, date.second) if !date.blank?
+    delivery_date = delivery_date + 1.hour
+
+    @order.date = delivery_date
+    if @accepted #payment successful
       @order.paid = true
-      @order.save
+      @order.agreed_to_terms = true
+      #@order.save
       redirect_to complete_orders_path
     else
       render 'show'
