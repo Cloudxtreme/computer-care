b93f949aa10fea8e34ff2423c04783a5c84097ea
Jason <jsofokleous@googlemail.com>
2013-10-20 12:17:32 +0100

    generate a random order and invoice number
    instead of sequential

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,12 @@
 class Invoice < ActiveRecord::Base
   belongs_to :order
   attr_accessible :order_id
+  before_create :randomize_id
+
+  private
+    def randomize_id
+      begin
+        self.id = SecureRandom.random_number(1_000_000)
+      end while Invoice.where(:id => self.id).exists?
+    end  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,12 @@ class Order < ActiveRecord::Base
   has_one :invoice
   has_one :student_code
   attr_accessible :building, :date, :email, :first_name, :last_name, :postcode, :street, :telephone, :total_cost, :town, :paid, :agreed_to_terms
+  before_create :randomize_id
+
+  private
+    def randomize_id
+      begin
+        self.id = SecureRandom.random_number(1_000_000)
+      end while Order.where(:id => self.id).exists?
+    end
 end
