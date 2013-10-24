62aa8bba2da41f1b18d68021f2a17d228a1931ae
Jason <jsofokleous@googlemail.com>
2013-10-24 01:34:13 +0100

    payment form styling

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,6 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @errors = []
     session["first-name"] = params["first-name"]
     session["last-name"] = params["last-name"]
     session["email"] = params["email"]
@@ -44,6 +45,11 @@ class OrdersController < ApplicationController
       delivery_date = delivery_date + 1.hour
     else
       delivery_date = params["complete_date"]
+    end
+
+    if params[:options].empty?
+      redirect_to new_order_path, :alert => "Please make sure you have selected a service before continuing"
+      return
     end
 
     @order = Order.new({:date => delivery_date, :building => params[:building], :email => params[:email], :first_name => params["first-name"], :last_name => params["last-name"], :postcode => params["postcode"], :street => params["street"], :telephone => params["telephone"], :town => params["town"]})    
@@ -76,12 +82,12 @@ class OrdersController < ApplicationController
       @invalid << "email"
       render :new
     else
-      @accepted = true
       render :confirm
     end
   end
 
   def finalize
+    @errors = []
     @options = params[:options]
     @order = Order.new(params[:order])
     @discount = nil
@@ -90,90 +96,87 @@ class OrdersController < ApplicationController
       session["discount"] = params[:discount] if @discount && @discount.is_valid
     end
     @services = Service.all
-    @missing = []
-    @invalid = []   
-    @accepted = true 
-    if !params[:accept_terms].blank? 
-      @order.agreed_to_terms = true
-      
-      total = 0.0
-      params[:options].each do |service_id, options|
-        service = Service.find(service_id)
-        total += service.base_cost
+    
+    total = 0.0
+    params[:options].each do |service_id, options|
+      service = Service.find(service_id)
+      total += service.base_cost
 
-        options.each do |service_option, value|
-          option = ServiceOption.find(service_option)
+      options.each do |service_option, value|
+        option = ServiceOption.find(service_option)
 
-          if !option.is_arbitrary
-            option_value = ServiceOptionValue.find(value)
-            total += option_value.additional_cost
-          end
+        if !option.is_arbitrary
+          option_value = ServiceOptionValue.find(value)
+          total += option_value.additional_cost
         end
       end
-
-      @order.total_cost = total
-
-      if params[:discount]
-        @discount = StudentCode.where("code = '#{params[:discount]}'").first 
-        if @discount
-          @order.total_cost = @order.total_cost - (@order.total_cost * 0.15)
-          @order.student_code_id = @discount.id
-          @discount.is_valid = false
-          @discount.save
-        end
-      end
-
-      if params[:options].any?
-        stripe_token = params["stripe-token"]
-        begin
-          response = Stripe::Charge.create(
-            :amount => @order.total_cost * 100,
-            :currency => "gbp",
-            :card => stripe_token,
-            :description => "Cheaper Computer Care"
-          )
-        rescue Stripe::CardError => e
-          #render :confirm
-          redirect_to new_order_path(:back => "true"), :alert => e.message
-          return
-        rescue Stripe::InvalidRequestError => e
-          #render :confirm
-          redirect_to new_order_path(:back => "true"), :alert => e.message
-          return
-        rescue Exception => e
-          #render :confirm
-          redirect_to new_order_path(:back => "true"), :alert => e.message
-          return      
-        end
-
-        if response.paid
-          @order.paid = true
-          @order.stripe_charge_id = response.id
-          @order.save
-
-          params[:options].each do |service_id, options|
-            order_service = @order.order_services.create(:service_id => service_id)
-            options.each do |option_id, value|
-              service_option = order_service.order_service_options.create(:service_option_id => option_id, :value => value)
-            end
-          end
-
-          # send confimation to customer
-          NotificationMailer.order_confirmation(@order).deliver
-          # send notification to admin
-          NotificationMailer.order_notification(@order).deliver
-
-          redirect_to complete_orders_path
-        else
-          render :confirm
-        end
-      else
-        render :new
-      end
-    else
-      @accepted = false
-      render :confirm
     end
+
+    @order.total_cost = total
+
+    if params[:discount]
+      @discount = StudentCode.where("code = '#{params[:discount]}'").first 
+      if @discount
+        @order.total_cost = @order.total_cost - (@order.total_cost * 0.15)
+        @order.student_code_id = @discount.id
+        @discount.is_valid = false
+        @discount.save
+      end
+    end
+
+    @stripe_token = params["stripe-token"]
+    begin
+      response = Stripe::Charge.create(
+        :amount => @order.total_cost * 100,
+        :currency => "gbp",
+        :card => @stripe_token,
+        :description => "Cheaper Computer Care"
+      )
+    rescue Stripe::CardError => e
+      @errors << "Something went wrong with your card payment: '#{e.message}'"  
+      @errors << "Please try again"
+      render :confirm
+      return      
+    rescue Stripe::InvalidRequestError => e
+      if !e.message.include?("You cannot use a Stripe token more than once")
+        @errors << "Something went wrong with your card payment: '#{e.message}'"
+        @errors << "Please try again and contact us if the problem persists"
+      end
+      render :confirm
+      return
+    end
+
+    if !response.paid
+      @errors << "Something went wrong with your card payment: '#{response.message}'"
+      @errors << "Please try again"
+      render :confirm
+      return
+    end
+
+    if params[:accept_terms].blank?
+      @errors << "The terms and conditions must be accepted before  continuing"
+      render :confirm
+      return      
+    end 
+
+    @order.paid = true
+    @order.stripe_charge_id = response.id
+    @order.agreed_to_terms = true
+    @order.save
+
+    params[:options].each do |service_id, options|
+      order_service = @order.order_services.create(:service_id => service_id)
+      options.each do |option_id, value|
+        service_option = order_service.order_service_options.create(:service_option_id => option_id, :value => value)
+      end
+    end
+
+    # send confimation to customer
+    NotificationMailer.order_confirmation(@order).deliver
+    # send notification to admin
+    NotificationMailer.order_notification(@order).deliver
+
+    redirect_to complete_orders_path
   end
 
   def complete
