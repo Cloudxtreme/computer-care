663c6609628739769f5be19187f8c94341587059
Jason <jsofokleous@googlemail.com>
2013-10-27 10:33:21 +0000

    format the date and year select boxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,8 +41,30 @@ module ApplicationHelper
         end
     end
 
-  
-  def snippet(text, wordcount=10) 
-    text.split[0..(wordcount-1)].join(" ") + (text.split.size > wordcount ? "..." : "") 
-  end
+    def snippet(text, wordcount=10) 
+        text.split[0..(wordcount-1)].join(" ") + (text.split.size > wordcount ? "..." : "") 
+    end
+
+    def expiry_month_options
+        "<option value='1'>01</option>" +
+        "<option value='2'>02</option>" +
+        "<option value='3'>03</option>" +
+        "<option value='4'>04</option>" +
+        "<option value='5'>05</option>" +
+        "<option value='6'>06</option>" +
+        "<option value='7'>07</option>" +
+        "<option value='8'>08</option>" +
+        "<option value='9'>09</option>" +
+        "<option value='10'>10</option>" +
+        "<option value='11'>11</option>" +
+        "<option value='12'>12</option>".html_safe
+    end
+
+    def expiry_year_options
+        options = ""
+        16.times do |i|
+            options += "<option value='#{Date.today.strftime('%y').to_i + i + 2000}'>#{Date.today.strftime('%y').to_i + i}</option>"
+        end
+        options.html_safe
+    end
 end
