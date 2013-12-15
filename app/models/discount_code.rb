cfbae18f0a448ce0722ea7f6bc87ad1d2c9b6df1
Jason <jsofokleous@googlemail.com>
2013-12-15 12:24:14 +0000

    limit discount percentage to 100%

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -2,6 +2,7 @@ class DiscountCode < ActiveRecord::Base
   attr_accessible :code, :is_valid, :discount
   validates_presence_of :discount
   validates_numericality_of :discount
+  validates :discount, :inclusion => 1..100
 
   before_create :set_valid, :generate_code
 
